64a261e2afc611222043325bf099ba39f38ac0fb
CristinaAndreeaM <cmititean147@gmail.com>
2022-01-19 19:28:27 +0200

    Formurile pt meniu

---
+++ FirstWindow/Carne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWindow
+{
+    public partial class Carne : Form
+    {
+        public Carne()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- FirstWindow/Form1.Designer.cs
+++ FirstWindow/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comandabtn = new System.Windows.Forms.Button();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comandabtn
+            // 
+            this.comandabtn.Location = new System.Drawing.Point(79, 110);
+            this.comandabtn.Name = "comandabtn";
+            this.comandabtn.Size = new System.Drawing.Size(121, 49);
+            this.comandabtn.TabIndex = 0;
+            this.comandabtn.Text = "Comanda noua";
+            this.comandabtn.UseVisualStyleBackColor = true;
+            this.comandabtn.Click += new System.EventHandler(this.comandabtn_Click);
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(285, 110);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(121, 49);
+            this.exitbtn.TabIndex = 1;
+            this.exitbtn.Text = "Exit";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            this.exitbtn.Click += new System.EventHandler(this.exitbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(500, 257);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.comandabtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button comandabtn;
+        private System.Windows.Forms.Button exitbtn;
     }
 }
 
