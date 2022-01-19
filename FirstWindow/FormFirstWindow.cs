b41659c9448bd7053db57c89fd3fcf6ebfce3fdf
DARIUS\Darius <vararean04darius@gmail.com>
2022-01-19 21:10:46 +0200

    greseala

--- FirstWindow/FormFirstWindow.cs
+++ FirstWindow/FormFirstWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FirstWindow
 {
-    public partial class Form1 : Form
+    public partial class FormFirstWindow : Form
     {
-        public Form1()
+        public FormFirstWindow()
         {
             InitializeComponent();
         }
